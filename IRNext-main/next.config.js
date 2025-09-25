916adc3ce30bc149f8411badb0e6b493fd5c2f9e
Syeda Juveria Afreen <sja.affu765@gmail.com>
2025-09-25 08:14:26 +0300

    update build

--- IRNext-main/next.config.js
+++ IRNext-main/next.config.js
@@ -1,21 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: false,
-    images: {
-        remotePatterns: [
-            {
-                hostname: "**",
-            }
-        ],
-    },
-    webpack: (config, { isServer }) => {
-        if (!isServer) {
-            const timestamp = Date.now(); // Generate unique timestamp
-            config.output.filename = `static/chunks/[name].[contenthash].${timestamp}.js`;
-            config.output.chunkFilename = `static/chunks/[name].[contenthash].${timestamp}.js`;                    
-        }
-        return config;
-    },
-}
+  reactStrictMode: false,
+  images: {
+    remotePatterns: [
+      {
+        hostname: '**',
+      },
+    ],
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      const timestamp = Date.now(); // Generate unique timestamp
+      config.output.filename = `static/chunks/[name].[contenthash].${timestamp}.js`;
+      config.output.chunkFilename = `static/chunks/[name].[contenthash].${timestamp}.js`;
+    }
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
